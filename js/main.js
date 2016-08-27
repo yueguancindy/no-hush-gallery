ab961f270c18cab910eaae65a606d36e6ed677d2
yueguancindy <guany613@newschool.edu>
2016-08-27 18:35:33 -0400

    nohush

---
+++ js/main.js
@@ -0,0 +1,132 @@
+
+
+
+document.getElementById('container1').onmousedown= function() {
+	
+	var thissound = document.getElementById('audio1');
+	// thissound.currentTime = 0;
+	thissound.play();
+
+}
+
+document.getElementById('container1').onmouseup = function() {
+	var thissound = document.getElementById('audio1');
+	thissound.pause();
+	thissound.currentTime = 0;
+}
+
+document.getElementById('container2').onmousedown= function() {
+	var thissound = document.getElementById('audio2');
+	// thissound.currentTime = 0;
+	thissound.play();
+}
+
+document.getElementById('container2').onmouseup = function() {
+	var thissound = document.getElementById('audio2');
+	thissound.pause();
+	thissound.currentTime = 0;
+}
+
+document.getElementById('container3').onmousedown= function() {
+	var thissound = document.getElementById('audio3');
+	// thissound.currentTime = 0;
+	thissound.play();
+}
+
+document.getElementById('container3').onmouseup = function() {
+	var thissound = document.getElementById('audio3');
+	thissound.pause();
+	thissound.currentTime = 0;
+}
+
+document.getElementById('container4').onmousedown= function() {
+	var thissound = document.getElementById('audio4');
+	// thissound.currentTime = 0;
+	thissound.play();
+}
+
+document.getElementById('container4').onmouseup = function() {
+	var thissound = document.getElementById('audio4');
+	thissound.pause();
+	thissound.currentTime = 0;
+}
+
+document.getElementById('container5').onmousedown= function() {
+	var thissound = document.getElementById('audio5');
+	// thissound.currentTime = 0;
+	thissound.play();
+}
+
+document.getElementById('container5').onmouseup = function() {
+	var thissound = document.getElementById('audio5');
+	thissound.pause();
+	thissound.currentTime = 0;
+}
+
+document.getElementById('container6').onmousedown= function() {
+	var thissound = document.getElementById('audio6');
+	// thissound.currentTime = 0;
+	thissound.play();
+}
+
+document.getElementById('container6').onmouseup = function() {
+	var thissound = document.getElementById('audio6');
+	thissound.pause();
+	thissound.currentTime = 0;
+}
+
+
+
+
+
+// test scroll button
+
+
+
+
+// TEST 1
+
+// container.onclick = function(e) {
+// 	if(document.getElementById('container1')) {
+// 		var thissound = document.getElementById('audio1');
+// 		thissound.currentTime = 0;
+// 		thissound.play();
+// 	}
+// 	if(document.getElementById('container2')) {
+// 		var thissound = document.getElementById('audio2');
+// 		thissound.currentTime = 0;
+// 		thissound.play();
+// 	}
+// }
+
+// TEST 1 END
+
+// function play(soundobj){
+// 	if(document.getElementById('container1')) {
+// 			soundobj = document.getElementById('audio1');
+// 	soundobj.currentTime = 0;
+
+// 	soundobj.play();
+// 	}
+
+// 	if(document.getElementById('container2')) {
+// 			var audio = document.getElementById('audio2');
+// 	audio.currentTime = 0;
+
+// 	audio.play();
+// 	}
+
+
+
+// 	// var audio = document.getElementById('audio1');
+// 	// audio.currentTime = 0;
+
+// 	// audio.play();
+
+
+// }
+
+// function pause(){
+//   var audio = document.getElementById('audio1')
+//   audio.pause();
+// }
